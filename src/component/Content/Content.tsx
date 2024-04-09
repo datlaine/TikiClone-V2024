2a722b0f6705199115cebc6c56bd11dcb03601bc
Lại Huỳnh Phát Đạt <136949840+datlaine@users.noreply.github.com>
2024-04-09 21:10:49 +0700

    Update Content.tsx

--- src/component/Content/Content.tsx
+++ src/component/Content/Content.tsx
@@ -52,7 +52,7 @@ const Content = () => {
                         ListProducts={<ProductGenuineBrand />}
                   />
 
-                  <ContentBook />
+{/*                   <ContentBook /> */}
                   <ContentFood />
 
                   <ContentProduct />
